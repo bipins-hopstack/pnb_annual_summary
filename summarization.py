46925db525216666b7da17a5a2f835819e5d2dec
bipins-hopstack <Bipinsreddy@gmail.com>
2024-06-21 18:17:48 +0530

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
-
-
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 from PIL import Image
-
-
-# In[49]:
-
-
-# To get one day ago details
-
-# current_datetime = datetime.now() - timedelta(days=1)
-# formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
-# Get current date and time
-#current_datetime = datetime.now().strftime("%Y-%m-%d")
-
-
-# In[50]:
-
 
 def add_logo(logo_path, width, height):
     """Read and return a resized logo"""
@@ -32,10 +13,6 @@ def add_logo(logo_path, width, height):
     return modified_logo
 
 
-# In[51]:
-
-
-# Streamlit UI
 st.title("Bank Report Summary")
 
 # Sidebar with collapsible section for RBI News
@@ -47,6 +24,7 @@ import streamlit as st
 import pandas as pd
 
 # Sidebar section for menu items
+st.sidebar.image(add_logo(logo_path=r"C:\Users\5173707\News\PNBLogo.png", width=175, height=85)) 
 st.sidebar.title("Select Bank and Report Type")
 
 # Define the options for banks and report types
